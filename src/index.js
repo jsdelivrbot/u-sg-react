8a04b201aec16ce1297e71b3d2267f40d3c9e3d9
Cristian <bufface@gmail.com>
2017-04-14 15:39:16 -0300

    14. Export Statements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const API_KEY = AIzaSyA_8mO_DqutP0mJ_ZaFCdVKBXQYVEnCvJQ;
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyA_8mO_DqutP0mJ_ZaFCdVKBXQYVEnCvJQ';
 
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
-  return <div>Hi!</div>;
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
