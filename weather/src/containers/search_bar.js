adf38d778368b9fe8ff3752bd184bad8acf536f9
Cristian <bufface@gmail.com>
2017-04-19 21:56:12 -0300

    53. Controlled Component and Binding Context

--- weather/src/containers/search_bar.js
+++ weather/src/containers/search_bar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+   console.log(event.target.value);
+   this.setState({ term: event.target.value});
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a five days forecast in your favorites cities."
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+          />
         <span className="input-group-btn">
           <button className="btn btn-secondary" type="submit">Submit</button>
         </span>
