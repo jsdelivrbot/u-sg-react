8283a29d09029d33c23088fba3bc6f650239265c
Cristian <bufface@gmail.com>
2017-04-23 16:30:23 -0300

    57. Ajax Requests with Axios

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'a351582c9aef4a6936ad1c3778a3607d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},arg`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
